8686571f52f73343e67bacdd25b746089c2f6e43
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-09-19 17:22:15 -0400

    Add Comments

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.addEventListener('keydown', playSound);
-
+//Play the sound
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -8,12 +7,14 @@ function playSound(e) {
     audio.play();
     key.classList.add('playing');
 }
-
+// Remove the playing class
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip it if itsd not transform
 
     this.classList.remove('playing');
 }
-
+// Select the transition from the key class and listen for the end
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// play sound when the key goes down
+window.addEventListener('keydown', playSound);
