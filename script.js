72be2529388e6800b33ab6a9b73250ab164b6550
Jean 'Jeazy' Felisme <jeazy@bizdotsmedia.com>
2018-09-19 17:11:58 -0400

    Clean up
    Created a playSound function standalone.

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.addEventListener('keydown', function(e) {
+window.addEventListener('keydown', playSound);
+
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return; //stop the function from running
     audio.currentTime = 0; // Rewind Time to play over with every hit
     audio.play();
     key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip it if itsd not transform
